0d106698e6d43e8f585ff594dac4bede44fc5dcb
Umar280408 <fillibrawl2@gmail.com>
2022-01-19 16:58:33 +0600

    Initial commit

---
+++ warror.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Warrior extends Hero{
+    public void applySuperAbility(String superAbilityTipy) {
+        System.out.println("Warrior применил способность АЛЛАКИ КОДЖИ");
+    }
+}
